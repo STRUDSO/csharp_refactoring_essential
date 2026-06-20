1b3465e975c5fe397e1514675159b38fac4be556
Dirk-Jan Swagerman <dirkjan@buffadoo.nl>
2026-06-20 14:12:54 +0200

    move tcr.sh to root and fix project paths

--- tcr.sh
+++ tcr.sh
@@ -3,13 +3,13 @@ cd "$(dirname "$0")"
 
 echo "Running LegacyCode.Console for multiple orderIds..."
 
-dotnet run --project ./LegacyCode.Console.csproj -- 1001
+dotnet run --project ./LegacyCode.Console/LegacyCode.Console.csproj -- 1001
 echo "----------------------------------------"
 
-dotnet run --project ./LegacyCode.Console.csproj -- 1002
+dotnet run --project ./LegacyCode.Console/LegacyCode.Console.csproj -- 1002
 echo "----------------------------------------"
 
-dotnet run --project ./LegacyCode.Console.csproj -- 1003
+dotnet run --project ./LegacyCode.Console/LegacyCode.Console.csproj -- 1003
 echo "----------------------------------------"
 
 echo "Done."
