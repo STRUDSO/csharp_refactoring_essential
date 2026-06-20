5a0b9c32b3b27bdb963d2d60afcf517b9440a213
Søren Trudsø Mahon <soren.mahon@m10s.io>
2026-06-20 12:31:45 +0200

    Add legacy project reference and stub for ShippingCalculator tests

---
+++ Comments.Test/ShippingCalculatorTest.cs
@@ -0,0 +1,15 @@
+using LegacyCode;
+
+namespace Comments.Test;
+
+[TestFixture]
+[TestOf(typeof(ShippingCalculator))]
+public class ShippingCalculatorTest
+{
+
+    [Test]
+    public void METHOD()
+    {
+        
+    }
+}
